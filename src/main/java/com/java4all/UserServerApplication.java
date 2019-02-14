9c4bfb1f29b70c7b3eab182882c5876864c66d0a
ITyunqing <you@example.com>
2019-02-14 14:04:46 +0800

    add info

--- src/main/java/com/java4all/UserServerApplication.java
+++ src/main/java/com/java4all/UserServerApplication.java
@@ -8,12 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
-
+/**
+ * 按请求粒度负载均衡(使用MongoDB存储事务日志):需引入SpringCloudConfiguration;
+ * 按事务粒度负载均衡(使用文件系统存储事务日志):需引入SpringCloudSecondaryConfiguration;
+ */
 @EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "com.java4all")
 @MapperScan("com.java4all.dao")
 @Import(SpringCloudSecondaryConfiguration.class)
 @EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class})
+//使用文件存储时，不需要配置mongodb
 public class UserServerApplication {
 
 	public static void main(String[] args) {
